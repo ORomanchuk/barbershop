636f08f3806924acfba36b274ed3cb6bde50938b
Oleksandr Romanchuk <o.romanchuk95@gmail.com>
2023-03-02 03:57:57 +0200

    fix some mistakes

--- pages/script/script.js
+++ pages/script/script.js
@@ -1,5 +1,42 @@
 let barbers = document.querySelector(".barbers");
 
+function setCursorPosition(pos, e) {
+    e.focus();
+    if (e.setSelectionRange) e.setSelectionRange(pos, pos);
+    else if (e.createTextRange) {
+        var range = e.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
+    }
+}
+
+function mask(e) {
+    let matrix = this.placeholder,
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+    def.length >= val.length && (val = def);
+    matrix = matrix.replace(/[_\d]/g, function (a) {
+        return val.charAt(i++) || "_"
+    });
+    this.value = matrix;
+    i = matrix.lastIndexOf(val.substr(-1));
+    i < matrix.length && matrix != this.placeholder ? i++ : i = matrix.indexOf("_");
+    setCursorPosition(i, this)
+}
+
+let phoneInput = document.querySelector(".phoneInput")
+
+
+phoneInput.addEventListener("click", function () {
+    let input = document.querySelector("#online_phone");
+    input.addEventListener("input", mask, false);
+    input.focus();
+    setCursorPosition(3, input);
+});
+
 class Barber {
     constructor(photo, name, ph1, ph2, ph3, ph4, ph5, ph6, ph7, ph8) {
         this.photo = photo;
